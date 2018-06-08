c6aaef01cf7fadd8b15b05bd867c0198108bcd6b
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-06-08 16:17:43 -0500

    Making progress with testing

--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,11 @@ const add  = (a, b) => {
     return a + b;
 }
 
+const square  = (a) => {
+    return a * a;
+}
+
 module.exports = {
-    add
+    add,
+    square
 }
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,9 +1,15 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add 2 numbers', () => {
     const res = utils.add(33, 11);
 
-    if(res != 44) {
-        throw new Error(`Expected 44 but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
+
+it('should square a number', () => {
+    const res = utils.square(3);
+
+    expect(res).toBe(9).toBeA('number');
+})
