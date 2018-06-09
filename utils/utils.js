7a3d83a7c510d51773fcbc8c060f5a8fb03c1b9e
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-06-09 15:15:03 -0500

    Finished async testing

--- utils/utils.js
+++ utils/utils.js
@@ -2,11 +2,26 @@ const add  = (a, b) => {
     return a + b;
 }
 
+const asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 1000);
+}
+
 const square  = (a) => {
     return a * a;
 }
 
+const setName = (user, fullName) => {
+    let names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+}
+
 module.exports = {
     add,
-    square
+    square,
+    setName,
+    asyncAdd
 }
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,8 +8,39 @@ it('should add 2 numbers', () => {
     expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add to two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    });
+});
+
 it('should square a number', () => {
     const res = utils.square(3);
 
     expect(res).toBe(9).toBeA('number');
-})
+});
+
+it('should expect some values', () => {
+    expect({name: 'boi'}).toEqual({name: 'boi'});
+    expect({
+        name: 'Drew',
+        age: 200,
+        location: 'Philly'
+    }).toInclude({
+        age: 200
+    });
+});
+
+it('should verify first and last names are set', () => {
+    let user = {
+        location:'Philly',
+        age: 200
+    }
+    let res = utils.setName(user, 'Drew Mead');
+
+    expect(res).toInclude({
+        firstName: 'Drew',
+        lastName: 'Mead'
+    });
+});
